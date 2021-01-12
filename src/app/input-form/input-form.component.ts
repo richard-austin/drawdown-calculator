7e35d0ad4c71cbf6c0a77741c3133944f026c309
Richard Austin <richard.david.austin@gmail.com>
2021-01-12 19:57:18 +0000

    Get started with Angular Materials

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-input-form',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-form.component.scss']
 })
 export class InputFormComponent implements OnInit {
+  @ViewChild('fundAmount', { read: ElementRef }) fundAmountEl!: ElementRef<HTMLInputElement>;
+  @ViewChild('drawdownAmount', { read: ElementRef }) drawdownAmountEl!: ElementRef<HTMLInputElement>;
+  fundAmount!: number;
+  drawdownAmount!: number;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cancel() {
+    //  Console.log("Cancelled");
+  }
+
+  formSubmitted() {
+    this.fundAmount = parseInt(this.fundAmountEl?.nativeElement.value.toString())
+    this.drawdownAmount = parseInt(this.drawdownAmountEl?.nativeElement.value.toString())
+  }
 }
