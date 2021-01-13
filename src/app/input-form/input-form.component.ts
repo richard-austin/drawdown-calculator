61533cd1e10a94bad3e9051cd5bb8a10458a2d49
Richard Austin <richard.david.austin@gmail.com>
2021-01-13 19:29:19 +0000

    Added the table. Some work still to do on the formatting

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -19,6 +19,7 @@ export class InputFormComponent implements OnInit {
   requiredAnnualIncomeGrowth!: number;
 
   drawdownData: YearTotal[] = [];
+  displayedColumns: string[] = ['yearNum', 'remainingFunds', 'annualIncome'];
 
   constructor() { }
 
